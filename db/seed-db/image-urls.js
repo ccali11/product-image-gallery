a4a2bcf248287c388641d854679df6ed41398c5c
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-24 22:48:52 -0500

    Refactored getByMetal into getAll and getType, then tested

--- db/seed-db/image-urls.js
+++ db/seed-db/image-urls.js
@@ -3,18 +3,37 @@ const path = require('path');
 
 const urlFile = 's3urls.txt';
 
-const getUrlByMetal = async (metal = null) => {
-  if (!metal) {
-    return fs.promises.readFile(path.join(__dirname, urlFile))
-      .then((fileUrlBuffer) => {
-        const fileUrls = fileUrlBuffer.toString();
-        const urlsArr = fileUrls.split('\n');
-        return urlsArr;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-}
+// Names and corresponding directories from url
+const nameDir = {
+  'rose': '\/rg\/',
+  'white': '\/wg\/',
+  'yellow': '\/yg\/',
+  'platinum': '\/p\/'
+};
 
-exports.getUrlByMetal = getUrlByMetal;
+const getAll = async () => {
+  return fs.promises.readFile(path.join(__dirname, urlFile))
+  .then((fileUrlBuffer) => {
+    const fileUrls = fileUrlBuffer.toString();
+    const urlsArr = fileUrls.split('\n');
+    return urlsArr;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+};
+
+const getType = async (type) => {
+
+  return getAll()
+      .then(urlsArr => {
+        const typeArr = urlsArr.filter((url) => {
+          return (url.includes(type) || url.includes(nameDir[type]));
+        })
+        return typeArr;
+      });
+};
+
+exports.getAll = getAll;
+exports.getType = getType;
+exports.nameDir = nameDir;
