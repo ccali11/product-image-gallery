c1269b48242606b5d28c83df412ee594387f3113
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-25 21:23:28 -0500

    Added my first API endpoint and confirmed functionality in Postman

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
 const express = require('express');
+const path = require('path');
+const db = require('../db/index.js');
+const app = express();
+const port = 3030;
+
+app.use(express.json());
+
+app.get('/gallery/:product_id', (req, res) => {
+  // TODO return default images.
+  const pid = req.params.product_id;
+  db.getDefaults(pid)
+    .then((galleryUrls) => {
+      res.send(galleryUrls);
+      res.end();
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
