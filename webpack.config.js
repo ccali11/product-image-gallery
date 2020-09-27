ff82d9fbec556a31c4d4d03e08de9b59a1e338e3
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-27 16:24:21 -0500

    Installed @babel/presec-react and adjusted package and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,22 @@ module.exports = {
         use: 'css-loader',
       },
       {
-        test: /\.(js|jsx)$/,
+        test: /\.jsx$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
