32f2e942440d985a822637dccc0571e4ac6e8855
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-27 16:51:51 -0500

    Added comment to API endpoint

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const port = 3030;
 
 app.use(express.json());
 
+// This API endpoint returns a set of default URLs to populate the carousel
 app.get('/gallery/:product_id', (req, res) => {
-  // TODO return default images.
   const pid = req.params.product_id;
   db.getDefaults(pid)
     .then((galleryUrls) => {
