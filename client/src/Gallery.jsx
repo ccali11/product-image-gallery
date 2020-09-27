6846d8fde9bdab148a902271fadca172c230a6de
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-27 18:34:31 -0500

    Built a wireframe

--- client/src/Gallery.jsx
+++ client/src/Gallery.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
-let Gallery = () => {
+const Container = styled.div`
+  display: flex;
+  border: 1px solid black;
+  flex-direction: row;
+  justify-content: space-between;
+  `;
 
+const Carousel = styled.div`
+  display: flex;
+  border: 1px solid black;
+  flex-direction: column;
+`;
+
+const Thumb = styled.div`
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+  font-size: 100px;
+`;
+
+const ImageView = styled.div`
+  border: 1px solid black;
+  width: 40vw;
+  height: 80vh;
+`;
+
+const ProductOptions = styled.div`
+  width: 40vw;
+  border: 1px solid black;
+`;
+
+const Gallery = () => {
   return(
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Container>
+      <Carousel>
+        <Thumb>1</Thumb>
+        <Thumb>2</Thumb>
+        <Thumb>3</Thumb>
+      </Carousel>
+      <ImageView>
+        <h1>
+          ImageView
+        </h1>
+      </ImageView>
+      <ProductOptions>
+        <h1>
+          ProductOptions
+        </h1>
+      </ProductOptions>
+    </Container>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from './Gallery.jsx';
-import './Gallery.css';
 
 ReactDOM.render(<Gallery />, document.getElementById('gallery'));
