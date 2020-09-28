685b18836565cf5329ea6171a01dad1d76643639
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-28 09:52:47 -0500

    Streamlined Models.test.js

--- db/Models.test.js
+++ db/Models.test.js
@@ -1,13 +1,7 @@
 const { sync, Product, StaticImage, Image } = require('./Models.js');
 
 test('sync should synchronize all models', async () => {
-  try {
-    await sync()
-      .then((results) => {
-        expect(results).toEqual(['Product','StaticImage','Image']);
-      });
-  } catch (err) {
-    console.error(err);
-  }
+    const results = await sync();
+    expect(results).toEqual(['Product','StaticImage','Image']);
 });
 
