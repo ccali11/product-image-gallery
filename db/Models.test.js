cd91b96e154bcaa8ef3ea22febbd7b4bb7428192
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-28 09:55:26 -0500

    Restructured Models.Test.js adding a describe line

--- db/Models.test.js
+++ db/Models.test.js
@@ -1,7 +1,9 @@
 const { sync, Product, StaticImage, Image } = require('./Models.js');
 
-test('sync should synchronize all models', async () => {
-    const results = await sync();
-    expect(results).toEqual(['Product','StaticImage','Image']);
+describe('sequelize models in Models.js', () => {
+  test('sync should synchronize all models', async () => {
+      const results = await sync();
+      expect(results).toEqual(['Product','StaticImage','Image']);
+  });
 });
 
