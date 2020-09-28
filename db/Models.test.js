53f8245e1ea093ede0f9fe11b68753617618feeb
Charlie Sellers <CharlieJSellers@gmail.com>
2020-09-28 09:57:27 -0500

    added .skip to test since I got it to pass and no longer want it to run

--- db/Models.test.js
+++ db/Models.test.js
@@ -1,6 +1,6 @@
 const { sync, Product, StaticImage, Image } = require('./Models.js');
 
-describe('sequelize models in Models.js', () => {
+describe.skip('sequelize models in Models.js', () => {
   test('sync should synchronize all models', async () => {
       const results = await sync();
       expect(results).toEqual(['Product','StaticImage','Image']);
