4bdc131aa2fdbe9c4893a4a82e85a7cbbbdd3fd1
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-01 12:35:38 -0500

    Created thumb component

---
+++ client/src/components/Carousel/components/Thumb.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ThumbDiv = styled.button`
+  display: flex;
+  align-items: center;
+  border: 0;
+  background-color: white;
+  width: 80px;
+  height: 80px;
+  font-size: 60px;
+  margin-bottom: 15px;
+  margin-left: 5px;
+  margin-right: 5px;
+`;
+
+const Thumbnail = styled.img`
+  width: 100%;
+`;
+
+const Thumb = (props) => {
+
+  return (
+    <ThumbDiv>
+      <Thumbnail src={props.source} />
+    </ThumbDiv>
+  );
+};
+
+export default Thumb;
