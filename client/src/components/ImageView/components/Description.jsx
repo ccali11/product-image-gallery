a49132ccc0a8b6daa878a8ee6fce0ed3e52c7d87
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-02 13:10:03 -0500

    Description functions as desired

--- client/src/components/ImageView/components/Description.jsx
+++ client/src/components/ImageView/components/Description.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 const DescriptionDiv = styled.div`
-height: 20px;
-border: 1px solid black;
-text-align: center;
+  height: 20px;
+  text-align: center;
+  font-size: 13px;
+  margin-bottom: 10px;
 `;
 
 const renderDescription = (state) => {
   const { metal, cut, carat, productData } = state;
   const { name } = productData;
 
+  const fullNames = {
+    'yellow': 'Yellow Gold',
+    'platinum': 'Platinum',
+    'rose': 'Rose Gold',
+    'white': 'White Gold'
+  };
+
+  const capFirst = (string) => {
+    const firstCap = string.charAt(0).toUpperCase();
+    const final = firstCap + string.slice(-(string.length - 1));
+    return final;
+  };
+
+  const description = `${fullNames[metal]} ${name} Setting with a(n) ${capFirst(cut)} Cut Gem, Shown with a ${carat/100} Carat Stone`;
+
   return (
     <span>
-      Description
+      {description}
     </span>
   );
 }
@@ -24,7 +40,7 @@ const Description = (props) => {
 
   return(
     <DescriptionDiv>
-      {}
+      {renderDescription(state)}
     </DescriptionDiv>
   );
 };
