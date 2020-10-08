0ee3d52f6c7a83882980018abe5e019982ca8d62
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-08 15:32:49 -0500

    Page no longer flashes product 1 during render

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -26,7 +26,7 @@ const Container = styled.div`
   `;
 
 const Gallery = () => {
-  const [pid, setPid] = useState(1);
+  const [pid, setPid] = useState(window.location.pathname.slice(1));
   const [metal, setMetal] = useState('white');
   const [cut, setCut] = useState('asscher');
   const [carat, setCarat] = useState(150);
@@ -40,8 +40,7 @@ const Gallery = () => {
   });
 
   useEffect(() => {
-    const pathname = window.location.pathname.slice(1)
-    console.log(pathname);
+    const pathname = window.location.pathname.slice(1);
     if (pathname) { return setPid(window.location.pathname.slice(1)) }
   }, [])
 
