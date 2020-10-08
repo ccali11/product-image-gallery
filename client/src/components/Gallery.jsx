6351217ffa330826ce4061777b5b1081eaa9d111
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-08 17:22:27 -0500

    removed unnecessary code block.

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -26,7 +26,7 @@ const Container = styled.div`
   `;
 
 const Gallery = () => {
-  const [pid, setPid] = useState(1);
+  const [pid, setPid] = useState((window.location.pathname.slice(1)) ? window.location.pathname.slice(1) : 1);
   const [metal, setMetal] = useState('white');
   const [cut, setCut] = useState('asscher');
   const [carat, setCarat] = useState(150);
@@ -39,10 +39,10 @@ const Gallery = () => {
     ratingcount: 0
   });
 
-  useEffect(() => {
-    const pathname = window.location.pathname.slice(1);
-    if (pathname) { return setPid(window.location.pathname.slice(1)) }
-  }, [])
+  // useEffect(() => {
+  //   const pathname = window.location.pathname.slice(1);
+  //   if (pathname) { return setPid(window.location.pathname.slice(1)) }
+  // }, [])
 
   useEffect(() => {
     axios.get(`http://localhost:3030/gallery/${pid}/${metal}/${cut}/${carat}`)
